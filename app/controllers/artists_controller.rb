163d8721c5a4cdf255d8c89366309b1dda708167
Scott Key <scottdkey@gmail.com>
2020-02-26 16:51:57 -0700

    added crud actions for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,56 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
   def index
+    @artists = current_user.artists
   end
 
   def new
+    @artist = current_user.artists.new
+  end
+
+  def create
+    @artist = current_user.artists.new(artist_params)
+    if @artist.save
+      redirect_to artists_path
+    else
+      render :new
+    end
   end
 
   def show
+
   end
 
-  def _form
+  def edit
   end
+
+  def update
+    if @artist.update(artist_params)
+      redirect_to artists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+  private
+
+  def set_user
+  end
+
+  def set_artist
+    @artist = current_user.artists.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :genre)
+  end
+  def set_user
+
+  end
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,5 @@ class Artist < ApplicationRecord
   belongs_to :user
 
   has_many :songs
-  has_many :billboards, through: songs
+  has_many :billboards, through: :songs
 end
