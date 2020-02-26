7392fa6583fab8f65f18872fa584a3c3fcc14849
Scott Key <scottdkey@gmail.com>
2020-02-26 16:39:59 -0700

    Billboard crud actions stable

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.0'
 gem 'semantic-ui-sass'
+gem 'jquery-rails'
+gem 'bootstrap', '~> 4.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,18 +1,40 @@
 class BillboardsController < ApplicationController
-  before_action :set_user
   before_action :set_billboard, only: [:show, :edit, :update, :destroy]
   def index
-    @billboards = current_user.billboard
+    @billboards = current_user.billboards
   end
 
   def new
-    @billboard
+    @billboard = current_user.billboards.new
+  end
+
+  def create
+    @billboard = current_user.billboards.new(billboard_params)
+    if @billboard.save
+      redirect_to billboards_path
+    else
+      render :new
+    end
   end
 
   def show
+
   end
 
   def edit
+  end
+
+  def update
+    if @billboard.update(billboard_params)
+      redirect_to billboards_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @billboard.destroy
+    redirect_to billboards_path
   end
 
   private
@@ -21,11 +43,14 @@ class BillboardsController < ApplicationController
   end
 
   def set_billboard
-    @billboard = current_user.billboard.find(params[:id])
+    @billboard = current_user.billboards.find(params[:id])
   end
 
   def billboard_params
     params.require(:billboard).permit(:name, :region)
+  end
+  def set_user
+
   end
 
 end
